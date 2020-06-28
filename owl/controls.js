6f090bbf04402dff537915d6cc275e1b21899182
Shamsad Anam <57474743+shamsad-anam@users.noreply.github.com>
2020-06-28 15:13:22 +0600

    Add files via upload

---
+++ owl/controls.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    margin: 10,
+    nav: false,
+    loop: true,
+    items: 1,
+    autoplay: true,
+    interval: 2000,
+  });
+});
